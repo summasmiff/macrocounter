78eca89b64f0622484f44313fdb78515321c3ca4
Summer Smith <jessica.summer.smith@gmail.com>
2017-07-16 10:01:00 -0400

    Add React

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,109 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, IndexRoute, Route, Link, browserHistory, Redirect } from 'react-router';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">MacroCounter</h1>
+        <h4>Macros Remaining Today</h4>
+        <table>
+          <tbody>
+            <tr>
+              <th>Protein</th>
+              <th>Fat</th>
+              <th>Carbs</th>
+              <th>Fiber</th>
+            </tr>
+            <tr>
+              <th>110</th>
+              <th>53</th>
+              <th>107</th>
+              <th>25</th>
+            </tr>
+          </tbody>
+        </table>
+        <form id="entry">
+          <label htmlFor="food">Add food</label>
+          <input id="food" type="text" form="entry"></input>
+          <label htmlFor="protein">Protein</label>
+          <input id="protein" type="text" form="entry"></input>
+          <label htmlFor="fat">Fat</label>
+          <input id="fat" type="text" form="entry"></input>
+          <label htmlFor="carbs">Carbs</label>
+          <input id="carbs" type="text" form="entry"></input>
+          <input type="submit" value="Save"></input>
+        </form>
+        <h4>Add recent</h4>
+        <ul>
+          <li><button>Coffee</button></li>
+          <li><button>Greek yogurt 0%</button></li>
+          <li><button>Cottage Cheese</button></li>
+        </ul>
+        <table>
+          <tbody>
+            <tr>
+              <th>&nbsp;</th>
+              <th>Protein</th>
+              <th>Fat</th>
+              <th>Carbs</th>
+              <th>Fiber</th>
+            </tr>
+            <tr>
+              <td>Yogurt</td>
+              <td>22</td>
+              <td>0</td>
+              <td>9</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>Fruit</td>
+              <td>1</td>
+              <td>0</td>
+              <td>27</td>
+              <td>3</td>
+            </tr>
+            <tr>
+              <td>Omelette</td>
+              <td>12</td>
+              <td>10</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>Cheese</td>
+              <td>9</td>
+              <td>10</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>Protein scoop</td>
+              <td>24</td>
+              <td>1</td>
+              <td>3</td>
+              <td>0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+export class HomeContainer extends React.Component {
+  render() {
+    return <Dashboard />;
+  }
+}
 
-// import socket from "./socket"
+export function render(element) {
+  ReactDOM.render((
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HomeContainer}/>
+      </Route>
+    </Router>
+  ), element);
+}
